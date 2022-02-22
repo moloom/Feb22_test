1df99ed760141c65efe32b5575a0994b5e9e1c08
moloom <moxy520527@163.com>
2022-02-22 19:06:09 +0800

    firstConmmit

---
+++ src/main/java/com/mo/Feb22TestApplication.java
@@ -0,0 +1,13 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Feb22TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Feb22TestApplication.class, args);
+    }
+
+}
